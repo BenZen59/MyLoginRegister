ae78714df799d0e07c626258faeb7d87e3effcd1
BenZen59 <benoit.zenone@gmail.com>
2022-08-19 17:57:58 +0200

    add bcrypt and test err

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const mysql2 = require('mysql2');
 
 const app = express();
 const cors = require('cors');
+
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
 require('dotenv').config();
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_SCHEMA } = process.env;
@@ -19,28 +24,36 @@ const db = mysql2.createConnection({
 app.post('/register', (req, res) => {
   const { username } = req.body;
   const { password } = req.body;
-  db.query(
-    'INSERT INTO users(username, password) VALUES (?,?)',
-    [username, password],
-    (err, result) => {
-      res.send(err);
-    }
-  );
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    db.query(
+      'INSERT INTO users(username, password) VALUES (?,?)',
+      [username, hash],
+      (err, result) => {
+        res.send(err);
+      }
+    );
+  });
 });
 
 app.post('/login', (req, res) => {
   const { username } = req.body;
   const { password } = req.body;
   db.query(
-    'SELECT * FROM users WHERE username = ? AND password = ?',
-    [username, password],
+    'SELECT * FROM users WHERE username = ?',
+    username,
     (err, result) => {
       if (err) {
         res.send(err);
       } else if (result.length > 0) {
-        res.send(result);
+        bcrypt.compare(password, result[0].password, (error, response) => {
+          if (response) {
+            res.send(result);
+          } else {
+            res.send({ message: 'Wrong username/password combination' });
+          }
+        });
       } else {
-        res.send({ message: 'Wrong username/password combination' });
+        res.send({ message: "User doesn't exist" });
       }
     }
   );
